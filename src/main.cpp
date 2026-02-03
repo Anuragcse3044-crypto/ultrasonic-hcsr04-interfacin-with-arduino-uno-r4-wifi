2192982c91c882016ed080655935f3cc02e0f7d8
Anuragcse3044 <anuragcse3044@gmail.com>
2026-02-03 09:19:28 +0530

    Add pin configuration in Ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,16 @@ void setup() {
 }
 
 void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034/2;
+    Serial.print("Distance : ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(1000);
 // write your code here
 }
